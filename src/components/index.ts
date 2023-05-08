30095e1d88f06456a6188079fbad0cf8163990a7
Sofidis <sofia.dissegna93@gmail.com>
2023-05-08 17:00:33 +0100

    Removed unused files

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import SdButton from "./SdButton/SdButton";
-import SdIcon from "./SdIcon/SdIcon";
-import SdInteractable from "./SdInteractable/SdInteractable";
-
-export { SdButton, SdInteractable, SdIcon };
